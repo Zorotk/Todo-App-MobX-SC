1318e572ad51dcf5cb43fdc5549527ce8740c159
Blazor <zorotkov@rambler.ru>
2021-01-15 20:22:18 +0300

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,78 @@
+import  React from "react";
+import { render } from "react-dom";
+import { observer } from "mobx-react";
+import "./styles.css";
+import { Instance, types } from "mobx-state-tree";
+import {Header,Main, Todo, TodoItem} from './styles/header'
+
+
+
+const TodoStore = types
+  .model("TodoStore", {
+    name: "",
+    completed: true,
+    id: types.number,
+  })
+  .actions((self) => ({
+    toggleComleted() {
+      self.completed = !self.completed;
+    }
+  
+  }));
+
+const RootStore = types
+  .model("RootStore", {
+    todoStore: types.array(TodoStore),
+    name: "",
+  })
+  .actions((self) => ({
+    addTodo() {
+      self.todoStore.push({
+        name: self.name,
+        completed: false,
+        id: Math.random(),
+      });
+      self.name = ''
+    },
+    deleteTodo(todo:Instance<typeof TodoStore>) {
+      self.todoStore.remove(todo)
+    },
+      setName(title:string) {
+      self.name=title
+    }
+  }));
+
+const rootStore = RootStore.create({});
+
+const App = observer(() => {
+
+  return (
+    <div>
+      <div>
+        <Header>Todo</Header>
+        <Main>
+           <input
+        value={rootStore.name}
+        onChange={(e) => rootStore.setName(e.target.value)}
+        />
+  
+      <button onClick={() => rootStore.addTodo()}>add</button>
+      {rootStore.todoStore.map((u, i) => (
+        <Todo key={i}>
+          <TodoItem>{u.name}</TodoItem>
+          <input
+            checked={u.completed}
+            onChange={() => u.toggleComleted()}
+            type="checkbox"
+          />
+          <TodoItem onClick={()=>rootStore.deleteTodo(u)}>X</TodoItem>
+        </Todo>
+      ))}
+        </Main>
+           </div>
+    </div>
+  );
+});
+
+const rootElement = document.getElementById("root");
+render(<App />, rootElement);
